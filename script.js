ffbdeaa6f25f8a46e842c100873bfcbb337771e4
WellyT <wellysonthomas@hotmail.com>
2023-06-02 01:06:13 -0300

    block scrolling when photo is open

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let cta = document.getElementById("cta")
 let close = document.getElementById("close")
 cta.addEventListener("click", () => {
   document.getElementById("imgs").classList.remove("show")
-
+  document.querySelector("body").style.overflow = "hidden"
   cta.style.outline = "none"
 })
 close.addEventListener("click", () => {
   document.getElementById("imgs").classList.add("show")
+  document.querySelector("body").style.overflow = "auto"
 })
